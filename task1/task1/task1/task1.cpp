4a2260a7040bf6744ff489d4ea2a69c16508eb70
lyuxiaoyu <514163514@qq.com>
2017-10-09 13:19:36 +0800

    test opencv

--- task1/task1/task1/task1.cpp
+++ task1/task1/task1/task1.cpp
@@ -1,11 +1,19 @@
 // task1.cpp: 定义控制台应用程序的入口点。
 //
 
+#include <iostream>
+#include <opencv2\opencv.hpp>
+
 #include "stdafx.h"
+
+using namespace cv;
 
 
 int main()
 {
+	Mat img = imread("source/test.jpg");
+	namedWindow("test");
+	imshow("test",img);
     return 0;
 }
 
